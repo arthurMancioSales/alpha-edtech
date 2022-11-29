b7bfbcfce7728a9c7ea0fdc0be7dc8b7e8312228
arthurMancioSales <arthur_mancio@outlook.com>
2022-11-29 09:47:52 -0300

    add: conteúdo aula JS 14 adicionado

---
+++ javascript/Aula 14/modules/pages/brigadeiroPage.js
@@ -0,0 +1,30 @@
+import EventoCustomizado from "../customEvent.js"
+
+export default function Brigadeiros() {
+    const container = document.createElement("div")
+    container.className = "flex-column"
+
+    const title = document.createElement("p")
+    title.className = "title"
+    title.innerText = "Conheça nossos brigadeiros"
+
+    const paragraph = document.createElement("p")
+    paragraph.className = "text"
+    paragraph.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, expedita laborum perferendis officia magni itaque perspiciatis. Consequatur minus culpa asperiores, porro possimus dicta consectetur repudiandae temporibus quia, facere earum distinctio?"
+    
+
+    const homeBtn = document.createElement("input")
+    homeBtn.type = "button"
+    homeBtn.value = "página principal"
+    homeBtn.className = "button"
+    homeBtn.onclick = function() {
+        const route = EventoCustomizado('/')
+        window.dispatchEvent(route)
+    }
+
+    container.appendChild(title)
+    container.appendChild(paragraph)
+    container.appendChild(homeBtn)
+
+    return container
+}
